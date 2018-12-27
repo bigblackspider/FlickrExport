835e300418acd0d1f508e587e5d40e977eed30aa
Peter Bass <peter.bass@scancare.com.au>
2018-12-27 09:51:14 +0200

    Add project files.

---
+++ FlickrExport/FlickrManager.cs
@@ -0,0 +1,32 @@
+﻿using FlickrExport.Properties;
+using FlickrNet;
+
+namespace FlickrExport
+{
+    public sealed class FlickrManager
+    {
+        public static OAuthAccessToken OAuthToken
+        {
+            get => Settings.Default.OAuthToken;
+            set
+            {
+                Settings.Default.OAuthToken = value;
+                Settings.Default.Save();
+            }
+        }
+
+        public static Flickr GetInstance()
+        {
+            return new Flickr(Settings.Default.ApiKey, Settings.Default.ApiSecret);
+        }
+
+        public static Flickr GetAuthInstance()
+        {
+            return new Flickr(Settings.Default.ApiKey, Settings.Default.ApiSecret)
+            {
+                OAuthAccessToken = OAuthToken.Token,
+                OAuthAccessTokenSecret = OAuthToken.TokenSecret
+            };
+        }
+    }
+}
